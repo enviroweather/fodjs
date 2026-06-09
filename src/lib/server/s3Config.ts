c545b9ba74aa14aa1d3f0ca0c4ff82b4f2b8eae1
Pat Bills <billspat@msu.edu>
2026-06-09 09:42:37 -0400

    env var names conflct with netlify

--- src/lib/server/s3Config.ts
+++ src/lib/server/s3Config.ts
@@ -1,15 +1,15 @@
 import {
-	AWS_REGION,
-	AWS_ACCESS_KEY_ID,
-	AWS_SECRET_ACCESS_KEY,
+	FOD_AWS_REGION,
+	FOD_AWS_ACCESS_KEY_ID,
+	FOD_AWS_SECRET_ACCESS_KEY,
 	S3_BUCKET_NAME
 } from '$env/static/private';
 
 export const s3Config = {
-	region: AWS_REGION,
+	region: FOD_AWS_REGION,
 	credentials: {
-		accessKeyId: AWS_ACCESS_KEY_ID,
-		secretAccessKey: AWS_SECRET_ACCESS_KEY
+		accessKeyId: FOD_AWS_ACCESS_KEY_ID,
+		secretAccessKey: FOD_AWS_SECRET_ACCESS_KEY
 	},
 	bucketName: S3_BUCKET_NAME
 } as const;
