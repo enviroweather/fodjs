0cd3d183c723478b557f4b60ede89e0651bb0cf0
Pat Bills <billspat@msu.edu>
2026-06-09 08:09:17 -0400

    attempt to add debug instrument panel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,41 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 	import '$lib/assets/global.css';
+	import Inspect, {InspectOptionsProvider, type InspectOptions} from 'svelte-inspect-value'
+  	import { page } from '$app/state'
+  	import { dev } from '$app/environment'
 
 	let { children } = $props();
+
+
+	let inspectOptions = $state<InspectOptions>({
+    	renderIf: dev,
+    	expandLevel: 0,
+    	// other preferences
+  	})
+
+
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<InspectOptionsProvider options={inspectOptions}>
+  <Inspect.Panel values={{
+    // spread to access getters
+    page: { ...page },
+    // or just pass page data
+    pageData: page.data,
+	
+  }} />
+
+
+
+
+
+<main>
+	{@render children()}
+</main>
+
+</InspectOptionsProvider>
